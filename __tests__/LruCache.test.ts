04d7e713eee2d2024e10b0e778e9a36f1e711fb8
moko <57133526+qriquri@users.noreply.github.com>
2022-07-08 19:03:22 +0900

    要素の追加と値の取得のテストの整理

--- __tests__/LruCache.test.ts
+++ __tests__/LruCache.test.ts
@@ -6,15 +6,18 @@ describe(' 要素をMapのように追加でき、対応する値を取得でき
     beforeEach(() => {
         lru = new LRUCache();
     })
-    test('要素にないものを取得するとnullが返ってくる', () => {
-        // 実行&検証
-        expect(lru.get("a")).toEqual(null);
-    })
-    test('名前"a"に対応する値"dataA"を追加しでき、名前に"a"を指定して値を取得するとその値は"dataA"', () => {
+    test('名前"a"に対応する値"dataA"、名前"b"に対応する値"dataB"を追加し、名前に"a"を指定して値を取得するとその値は"dataA"', () => {
         // 要素を追加
         lru.put("a", "dataA");
-        
+        lru.put("b", "dataB")
         // 要素を取得&検証
         expect(lru.get("a")).toEqual("dataA");
+    })
+    test('名前"a"に対応する値"dataA"、名前"b"に対応する値"dataB"を追加し、名前に"c"を指定して値を取得するとその値はnull', () => {
+        // 要素を追加
+        lru.put("a", "dataA");
+        lru.put("b", "dataB")
+        // 要素を取得&検証
+        expect(lru.get("c")).toEqual(null);
     })
 })
