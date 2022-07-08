da582550e8e2f30de0d5a80bcabdc631f702bf4e
moko <57133526+qriquri@users.noreply.github.com>
2022-07-08 18:56:52 +0900

    要素を追加して、取得できるようにした

--- __tests__/LruCache.test.ts
+++ __tests__/LruCache.test.ts
@@ -2,10 +2,19 @@ import LRUCache from "../src/LRUCache"
 
 
 describe(' 要素をMapのように追加でき、対応する値を取得できる', () => {
+    let lru: LRUCache;
+    beforeEach(() => {
+        lru = new LRUCache();
+    })
     test('要素にないものを取得するとnullが返ってくる', () => {
-        // 前準備
-        const lru = new LRUCache();
         // 実行&検証
         expect(lru.get("a")).toEqual(null);
+    })
+    test('名前"a"に対応する値"dataA"を追加しでき、名前に"a"を指定して値を取得するとその値は"dataA"', () => {
+        // 要素を追加
+        lru.put("a", "dataA");
+        
+        // 要素を取得&検証
+        expect(lru.get("a")).toEqual("dataA");
     })
 })
--- src/LRUCache.ts
+++ src/LRUCache.ts
@@ -7,6 +7,17 @@ export default class LRUCache{
     private caches: ICache[] = [];
     
     /**
+     * 要素を追加する
+     * @param {string} name 
+     * @param {any} val 
+     */
+    public put(name: string, val: any){
+        const cache = {name: name, val: val};
+        // 要素を追加
+        this.caches.push(cache);
+    }
+
+    /**
      * 指定した名前に対応する値を取得する
      * @param {string} name 
      * @return 
